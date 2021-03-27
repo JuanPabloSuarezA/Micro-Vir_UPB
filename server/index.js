f7f94571e547c365f260691278f0e53a94759355
duvian <duvian.ariza.2016@upb.edu.co>
2021-03-27 16:15:02 -0500

    Index updates

--- server/index.js
+++ server/index.js
@@ -4,14 +4,20 @@ const bodyParser= require('body-parser')
 const multer = require('multer');
 const { v4: uuidv4 } = require('uuid');
 const path = require('path');
+const cors = require('cors');
 
-
+// errores
+const enoent = require('./middlewares/enoent');
+const eexist = require('./middlewares/eexist');
+const err = require('./middlewares/err');
 
 //Init
 const app = express();
 require('./db')
 
 //Middleware
+app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({extended: false}));
 
 const storage = multer.diskStorage({
@@ -25,11 +31,24 @@ app.use(multer({storage}).single('image'));
 
 
 //Load Routes
-const ImageRoutes = require('./routers/ImagenRoutes')
+const ImageRoutes = require('./routes/ImagenRoutes');
+const contentRouter = require('./routes/content');
+const uploadRouter = require('./routes/upload');
+const downloadRouter = require('./routes/download');
+const dirRouter = require('./routes/dir');
 
 //Routes
 app.use('/',ImageRoutes);
+app.use('/content', contentRouter);
+app.use('/upload', uploadRouter);
+app.use('/download', downloadRouter);
+app.use('/dir', dirRouter);
 
-const PORT = 4000;
+// Errors
+app.use(enoent);
+app.use(eexist);
+app.use(err);
+
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => console.log(`Server start on port ${PORT}`));
