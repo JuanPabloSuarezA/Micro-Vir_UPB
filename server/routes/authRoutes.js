26e76e056d4cc096be8a19d51ac9a220c211f6bc
JuanPabloSuarezA <jpsa4000@hotmail.com>
2021-03-27 18:49:16 -0500

    unable to add with last merge commit

---
+++ server/routes/authRoutes.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const { register, login } = require("../controllers/auth");
+
+router.route("/register").post(register);
+
+router.route("/login").post(login);
